f3d86e10c9e169f08479fae2506e6f7e3a019cb3
sarlor <kinsleer@outlook.com>
2018-09-05 10:47:17 +0800

    core/mempool: fix import & blockchian error

--- core/mempool/state_processor.go
+++ core/mempool/state_processor.go
@@ -1,19 +1,20 @@
 package mempool
 
 import (
-
+	"srcd/core/blockchain"
+	"srcd/consensus"
 )
 
 // StateProcessor is a basic Processor, which takes care of transitioning
 // state from one point to another.
 type StateProcessor struct {
 	// config *params.ChainConfig // Chain configuration options
-	bc     *BlockChain         // Canonical block chain
+	bc     *blockchain.BlockChain         // Canonical block chain
 	engine consensus.Engine    // Consensus engine used for block rewards
 }
 
 // NewStateProcessor initialises a new StateProcessor.
-func NewStateProcessor(bc *BlockChain, engine consensus.Engine) *StateProcessor {
+func NewStateProcessor(bc *blockchain.BlockChain, engine consensus.Engine) *StateProcessor {
 	return &StateProcessor{
 		// config: config,
 		bc:     bc,
