f1d9594e6f90aaab581aff73a51dce84618ae355
coolhackboy <571219013@qq.com>
2018-09-11 09:34:32 +0800

    delete orphan_manage

--- protocol/orphan_manage.go
+++
@@ -1,9 +0,0 @@
-package protocol
-
-//// OrphanManage is use to handle all the orphan block
-//type OrphanManage struct {
-//	//TODO: add orphan cached block limit
-//	orphan      map[bc.Hash]*types.Block
-//	prevOrphans map[bc.Hash][]*bc.Hash
-//	mtx         sync.RWMutex
-//}
