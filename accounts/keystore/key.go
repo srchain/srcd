d9cbbc78877992c432858861af5ab8705efacf2f
sarlor <kinsleer@outlook.com>
2018-09-12 17:10:16 +0800

    implement accounts

---
+++ accounts/keystore/key.go
@@ -0,0 +1,74 @@
+package keystore
+
+import (
+	"crypto/ecdsa"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"srcd/common/common"
+
+	"github.com/pborman/uuid"
+)
+
+type Key struct {
+	Id uuid.UUID // Version 4 "random" for unique id not derived from key data
+	// to simplify lookups we also store the address
+	Address common.Address
+	// we only store privkey as pubkey/address can be derived from it
+	// privkey in this struct is always in plaintext
+	PrivateKey *ecdsa.PrivateKey
+}
+
+type keyStore interface {
+	// Loads and decrypts the key from disk.
+	GetKey(addr common.Address, filename string, auth string) (*Key, error)
+	// Writes and encrypts the key.
+	StoreKey(filename string, k *Key, auth string) error
+	// Joins filename with the key directory unless it is already absolute.
+	JoinPath(filename string) string
+}
+
+type encryptedKeyJSON struct {
+	Address string     `json:"address"`
+	Crypto  cryptoJSON `json:"crypto"`
+	Id      string     `json:"id"`
+	Version int        `json:"version"`
+}
+
+type cryptoJSON struct {
+	Cipher       string                 `json:"cipher"`
+	CipherText   string                 `json:"ciphertext"`
+	CipherParams cipherparamsJSON       `json:"cipherparams"`
+	KDF          string                 `json:"kdf"`
+	KDFParams    map[string]interface{} `json:"kdfparams"`
+	MAC          string                 `json:"mac"`
+}
+
+type cipherparamsJSON struct {
+	IV string `json:"iv"`
+}
+
+func writeKeyFile(file string, content []byte) error {
+	// Create the keystore directory with appropriate permissions
+	// in case it is not present yet.
+	const dirPerm = 0700
+	if err := os.MkdirAll(filepath.Dir(file), dirPerm); err != nil {
+		return err
+	}
+
+	// Atomic write: create a temporary hidden file first
+	// then move it into place. TempFile assigns mode 0600.
+	f, err := ioutil.TempFile(filepath.Dir(file), "."+filepath.Base(file)+".tmp")
+	if err != nil {
+		return err
+	}
+	if _, err := f.Write(content); err != nil {
+		f.Close()
+		os.Remove(f.Name())
+		return err
+	}
+	f.Close()
+
+	return os.Rename(f.Name(), file)
+}
---
+++ accounts/keystore/keystore.go
@@ -0,0 +1,65 @@
+package keystore
+
+import (
+	"errors"
+	"path/filepath"
+	"sync"
+
+	"srcd/accounts"
+	"srcd/common/common"
+)
+
+var (
+	ErrDecrypt = errors.New("could not decrypt key with given passphrase")
+)
+
+// KeyStore manages a key storage directory on disk.
+type KeyStore struct {
+	storage     keyStore                     // Storage backend, might be cleartext or encrypted
+	// cache       *accountCache                // In-memory account cache over the filesystem storage
+	changes     chan struct{}                // Channel receiving change notifications from the cache
+	unlocked    map[common.Address]*unlocked // Currently unlocked account (decrypted private keys)
+
+	wallets     []accounts.Wallet            // Wallet wrappers around the individual key files
+	// updateFeed  event.Feed                   // Event feed to notify wallet additions/removals
+	// updateScope event.SubscriptionScope      // Subscription scope tracking current live listeners
+	// updating    bool                         // Whether the event notification loop is running
+
+	mu          sync.RWMutex
+}
+
+type unlocked struct {
+	*Key
+	abort chan struct{}
+}
+
+// NewKeyStore creates a keystore for the given directory.
+func NewKeyStore(keydir string, scryptN, scryptP int) *KeyStore {
+	keydir, _ = filepath.Abs(keydir)
+	ks := &KeyStore{storage: &keyStorePassphrase{keydir, scryptN, scryptP}}
+	ks.init(keydir)
+	return ks
+}
+
+func (ks *KeyStore) init(keydir string) {
+	// Lock the mutex since the account cache might call back with events
+	ks.mu.Lock()
+	defer ks.mu.Unlock()
+
+	// Initialize the set of unlocked keys and the account cache
+	ks.unlocked = make(map[common.Address]*unlocked)
+	// ks.cache, ks.changes = newAccountCache(keydir)
+
+	// // TODO: In order for this finalizer to work, there must be no references
+	// // to ks. addressCache doesn't keep a reference but unlocked keys do,
+	// // so the finalizer will not trigger until all timed unlocks have expired.
+	// runtime.SetFinalizer(ks, func(m *KeyStore) {
+		// m.cache.close()
+	// })
+	// // Create the initial list of wallets from the cache
+	// accs := ks.cache.accounts()
+	// ks.wallets = make([]accounts.Wallet, len(accs))
+	// for i := 0; i < len(accs); i++ {
+		// ks.wallets[i] = &keystoreWallet{account: accs[i], keystore: ks}
+	// }
+}
