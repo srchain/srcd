d238ab9a018db475a7bac1ffc28ac40da89832ab
sarlor <kinsleer@outlook.com>
2018-09-30 15:21:40 +0800

    core/types: add Body for block's data

--- core/types/block.go
+++ core/types/block.go
@@ -67,17 +67,19 @@ func rlpHash(x interface{}) (h common.Hash) {
 	return h
 }
 
+// Body is a simple (mutable, non-safe) data container for storing and moving
+// a block's data contents (transactions) together.
+type Body struct {
+	Transactions []*Transaction
+}
+
 type Block struct {
 	header       *Header
 	transactions Transactions
 
 	// caches
-	hash atomic.Value
-	size atomic.Value
-
-	// Td is used by package core to store the total difficulty
-	// of the chain up to and including the block.
-	// td *big.Int
+	hash         atomic.Value
+	size         atomic.Value
 
 	// These fields are used by package eth to track
 	// inter-peer block relay.
