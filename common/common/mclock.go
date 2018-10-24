7c72d2dfbfb525a8de1afecd9012369f686d9138
sarlor <kinsleer@outlook.com>
2018-10-24 14:59:01 +0800

    common/mclock: add mclock

---
+++ common/common/mclock.go
@@ -0,0 +1,13 @@
+package common
+
+import (
+	"time"
+
+	"github.com/aristanetworks/goarista/monotime"
+)
+
+type AbsTime time.Duration // absolute monotonic time
+
+func Now() AbsTime {
+	return AbsTime(monotime.Now())
+}
